ed21f963f83737970c2dded38ac44cd87dc6afdb
Anpanner <34072369+Anpanner@users.noreply.github.com>
2017-11-28 19:49:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.Mzg1MTMzMjYwNjY3MDkyOTky.DP867Q.yZ93mjw_rtkaI_rYPb_OoyHQVbg);
+client.login(process.env.BOT_TOKEN);
